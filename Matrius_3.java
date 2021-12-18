32a6277474d4828ae5881bd2cd8129eeba440788
ferrerjoel <95489740+ferrerjoel@users.noreply.github.com>
2021-12-18 14:04:01 +0100

    Update Matrius_3.java

--- Matrius_3.java
+++ Matrius_3.java
@@ -18,7 +18,7 @@ class Matrius_1{
       rowSum = 0;
     }
 
-    System.out.println("-------------------------------+");
+
     //Imprimim la fila horitzontal de sumes
     for (byte i = 0; i < 10; i++) {
       for (byte j = 0; j < 10; j++) {
